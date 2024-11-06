6f003fb0b0c9bbbf8ba86536eb2ae396c7c2a4fd
Vindyani1999 <k.a.c.h.vindyani@gmail.com>
2024-11-06 12:23:35 +0530

    temporary

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "http://localhost:5000/api", // Point to your API Gateway
 });
 
 // Set up Axios interceptor to automatically attach the token
